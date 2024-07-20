961b62146400838c36cabeec5f2931964071129d
Mirko Di Franco <120678275+mirko-di-franco@users.noreply.github.com>
2024-07-20 16:06:59 +0200

    entities

---
+++ src/entities/UserBookmark.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class UserBookmark {
+	private User user;
+	private Bookmark bookmark;
+
+//	GETTERS AND SETTERS
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Bookmark getBookmark() {
+		return bookmark;
+	}
+
+	public void setBookmark(Bookmark bookmark) {
+		this.bookmark = bookmark;
+	}
+
+}
---
+++ src/entities/WebLink.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class WebLink extends Bookmark {
+	private String url;
+	private String host;
+	
+//	GETTERS AND SETTERS
+	
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getHost() {
+		return host;
+	}
+	public void setHost(String host) {
+		this.host = host;
+	}
+	
+	
+	
+}
